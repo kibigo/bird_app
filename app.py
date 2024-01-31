8ad127b175f7ff10bc024224ac127688c88f8d3b
kibigo <jacksonkibigo99@gmail.com>
2024-01-31 21:04:28 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+
+from flask import Flask, jsonify, make_response
+from flask_migrate import Migrate
+from flask_restful import Api, Resource
+
+from models import db, Bird
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://jack:7mKd5NOhSsVVQQVoR5iw4MUHai5C3tta@dpg-cmt8ao8cmk4c738ljujg-a.oregon-postgres.render.com/bird_app_sf81'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+migrate = Migrate(app, db)
+db.init_app(app)
+
+api = Api(app)
+
+class Birds(Resource):
+
+    def get(self):
+        birds = [bird.to_dict() for bird in Bird.query.all()]
+        return make_response(jsonify(birds), 200)
+
+api.add_resource(Birds, '/birds')
